e5e6020e66d2eef7837be360767ccf619abe2e22
demouth <yuya@demouth.net>
2024-09-06 00:04:13 +0900

    add rotation

--- 019/main.go
+++ 019/main.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	_ "image/png"
 	"log"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
@@ -39,7 +40,6 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	s := g.shader
 
-	w, h := screen.Bounds().Dx(), screen.Bounds().Dy()
 	cx, cy := ebiten.CursorPosition()
 
 	op := &ebiten.DrawTrianglesShaderOptions{}
@@ -50,19 +50,52 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	vertices := []ebiten.Vertex{
 		{
-			DstX: float32(w / 2), DstY: float32(h/2) - 100,
+			DstX: 0, DstY: -100,
 			ColorR: 1, ColorG: 0, ColorB: 0, ColorA: 1,
+			Custom0: 0,
 		},
 		{
-			DstX: float32(w/2) - 150, DstY: float32(h/2) + 100,
+			DstX: -150, DstY: 100,
 			ColorR: 0, ColorG: 1, ColorB: 0, ColorA: 1,
+			Custom0: 0,
 		},
 		{
-			DstX: float32(w/2) + 150, DstY: float32(h/2) + 100,
+			DstX: 150, DstY: 100,
 			ColorR: 0, ColorG: 0, ColorB: 1, ColorA: 1,
+			Custom0: 0,
 		},
 	}
 	indices := []uint16{0, 1, 2}
+
+	w, h := screen.Bounds().Dx(), screen.Bounds().Dy()
+	cx, cy = cx-screenWidth/2, cy-screenHeight/2
+	thetaY := float64(cx) / 1000
+	thetaX := float64(cy) / 1000
+	for i := 0; i < len(vertices); i++ {
+		x, y, z := vertices[i].DstX, vertices[i].DstY, vertices[i].Custom0
+
+		// rotate Y
+		sin64, cos64 := math.Sincos(thetaY)
+		sin, cos := float32(sin64), float32(cos64)
+		x = x*cos - z*sin
+		z = z*cos + x*sin
+
+		// rotate X
+		sin64, cos64 = math.Sincos(thetaX)
+		sin, cos = float32(sin64), float32(cos64)
+		y = y*cos - z*sin
+		z = z*cos + y*sin
+
+		// perspective
+		fl := float32(550)
+		scale := fl / (fl + z)
+		x *= scale
+		y *= scale
+		vertices[i].DstX = x + float32(w/2)
+		vertices[i].DstY = y + float32(h/2)
+		vertices[i].Custom0 = z
+	}
+
 	screen.DrawTrianglesShader(vertices, indices, s, op)
 }
 
