84ea67bf3e6fb71cfe3bdde8aa03008d9f147048
demouth <yuya@demouth.net>
2024-09-15 00:14:41 +0900

    remove tdewolff/canvas

--- 025/main.go
+++ 025/main.go
@@ -5,16 +5,15 @@ import (
 	"fmt"
 	"image/color"
 	_ "image/png"
-	"io"
 	"log"
+	"math"
 	"math/rand"
 
-	"github.com/demouth/ebitengine-sketch/025/drawer"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
-	"github.com/tdewolff/canvas"
-	"golang.org/x/image/font/gofont/goregular"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 const (
@@ -24,81 +23,57 @@ const (
 
 var (
 	textFaceSource *text.GoTextFaceSource
-	segments       []canvas.Segment
 )
 
 func init() {
-	/*
-		// s, err := text.NewGoTextFaceSource(bytes.NewReader(fonts.PressStart2P_ttf))
-		s, err := text.NewGoTextFaceSource(bytes.NewReader(goregular.TTF))
-		if err != nil {
-			log.Fatal(err)
-		}
-		textFaceSource = s
-
-		b, err := io.ReadAll(bytes.NewReader(goregular.TTF))
-		if err != nil {
-			log.Fatal(err)
-		}
-		font_, err := truetype.Parse(b)
-		freetype.ParseFont(b)
-		if err != nil {
-			log.Fatal(err)
-		}
-		font_.VMetric(10, 10)
-		face := truetype.NewFace(font_, &truetype.Options{
-			Size: 16, // これがフォントサイズ。
-		})
-		face.Glyph(fixed.Point26_6{X: 1, Y: 2}, 'a')
-
-		// 描画用の構造体を準備する。
-		img := image.NewRGBA(image.Rect(0, 0, 256, 128))
-		d := &font.Drawer{
-			Dst:  img,
-			Src:  image.NewUniform(color.Black),
-			Face: face,
-			Dot:  fixed.Point26_6{X: fixed.I(30)},
-		}
-		d.DrawString("Hello, World! こんにちは、世界！\nThis is a test.")
-	*/
-	// path := vector.Path{}
-	// text.AppendVectorPath(path, "A")
-
-	fontFamily := canvas.NewFontFamily("goregular")
-	b2, err := io.ReadAll(bytes.NewReader(goregular.TTF))
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(fonts.MPlus1pRegular_ttf))
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := fontFamily.LoadFont(b2, 0, canvas.FontRegular); err != nil {
-		panic(err)
-	}
-	face__ := fontFamily.Face(14.0, canvas.Black, canvas.FontBold, canvas.FontNormal)
-	path, _, err := face__.ToPath("A")
-	if err != nil {
-		panic(err)
-	}
-	segments = path.Segments()
-	fmt.Println(segments)
+	textFaceSource = s
 }
 
 type Game struct {
-	segments []canvas.Segment
+	vertices   []ebiten.Vertex
+	indices    []uint16
+	whiteImage *ebiten.Image
+	time       int
 }
 
 func (g *Game) Update() error {
+	g.time++
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.NRGBA{0x66, 0x66, 0x66, 0xff})
 
-	for _, segment := range g.segments {
-		drawer.DrawLine(screen,
-			float32(segment.Start.X)*80+100+rand.Float32()*8, -float32(segment.Start.Y)*80+400+rand.Float32()*8,
-			float32(segment.End.X)*80+100+rand.Float32()*8, -float32(segment.End.Y)*80+400+rand.Float32()*8,
-			1,
-			color.RGBA{0xff, 0xff, 0xff, 0xff},
-		)
+	path := &vector.Path{}
+	{
+		face := &text.GoTextFace{Source: textFaceSource, Size: 200}
+		op := &text.LayoutOptions{}
+		op.LineSpacing = 200
+		text.AppendVectorPath(path, "寿司を\n食べた\nい", face, op)
+	}
+
+	g.vertices, g.indices = path.AppendVerticesAndIndicesForFilling(g.vertices[:0], g.indices[:0])
+
+	for i := range g.vertices {
+		g.vertices[i].DstX = g.vertices[i].DstX + float32(math.Sin(float64(float32(g.time)+g.vertices[i].DstX)/10))*4 + rand.Float32()*3
+		g.vertices[i].DstY = g.vertices[i].DstY + float32(math.Cos(float64(float32(g.time)+g.vertices[i].DstY)/14))*4 + rand.Float32()*3 - 30
+		g.vertices[i].SrcX = 1
+		g.vertices[i].SrcY = 1
+		g.vertices[i].ColorR = 0xff
+		g.vertices[i].ColorG = 0xff
+		g.vertices[i].ColorB = 0xff
+		g.vertices[i].ColorA = 1
+	}
+
+	{
+		op := &ebiten.DrawTrianglesOptions{}
+		op.FillRule = ebiten.FillRuleNonZero
+		op.AntiAlias = true
+		screen.DrawTriangles(g.vertices, g.indices, g.whiteImage, op)
 	}
 
 	ebitenutil.DebugPrint(screen, fmt.Sprintf(
@@ -112,12 +87,13 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
-	game := &Game{
-		segments: segments,
+	g := &Game{
+		whiteImage: ebiten.NewImage(3, 3),
 	}
+	g.whiteImage.Fill(color.White)
 	ebiten.SetWindowSize(screenWidth, screenHeight)
-	ebiten.SetWindowTitle("Ebitengine + Canvas")
-	if err := ebiten.RunGame(game); err != nil {
+	ebiten.SetWindowTitle("Ebitengine - outline text")
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
